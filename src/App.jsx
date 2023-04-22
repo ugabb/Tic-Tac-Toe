d2eaf7add1a8200bbed0aae3fbed0f492737bcd4
ugabb <gsb.alternativo@gmail.com>
2023-04-22 12:47:49 -0300

    sequence winner feedback added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
   const [board, setBoard] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
   const [turn, setTurn] = useState(1);
   const [winner, setWinner] = useState(null);
+  const [sequenceWinner, setSequenceWinner] = useState([]);
+  const [showWithDelay, setShowWithDelay] = useState(false);
   const [play, setPlay] = useState(false);
 
   const winnerCombinations = [
@@ -54,12 +56,19 @@ function App() {
       const { indexes } = combination;
 
       // if x win
+      // console.log(indexes[0],indexes[1],indexes[2]);
+
       if (
         board[indexes[0]] === 1 &&
         board[indexes[1]] === 1 &&
         board[indexes[2]] === 1
       ) {
         setWinner("❌");
+        setSequenceWinner((prev) => [...prev, indexes[0],indexes[1],indexes[2]]);
+        // winner alert will show with 2 seconds of delay
+        setTimeout(() => {
+          setShowWithDelay(true);
+        }, 2000);
       }
       // if o win
       if (
@@ -68,16 +77,27 @@ function App() {
         board[indexes[2]] === 2
       ) {
         setWinner("⭕");
+        setSequenceWinner((prev) => [...prev, indexes[0],indexes[1],indexes[2]]);
+        // winner alert will show with 2 seconds of delay
+        setTimeout(() => {
+          setShowWithDelay(true);
+        }, 2000);
       }
       if (winner) break;
     }
     return winner;
   }
 
+  useEffect(() => {
+    console.log("Sequence winner:", sequenceWinner);
+  }, [sequenceWinner]);
+
   function resetGame() {
     setBoard([0, 0, 0, 0, 0, 0, 0, 0, 0]);
     setTurn(1);
     setWinner(null);
+    setShowWithDelay(false);
+    setSequenceWinner([])
   }
 
   useEffect(() => {
@@ -100,7 +120,7 @@ function App() {
         <div className="board-container">
           {board.map((value, index) => (
             <span
-              className={`${value ? "alreadyPlaced" : ""}`}
+              className={`${value ? "alreadyPlaced" : ""} ${sequenceWinner.includes(index) ? 'winnerSequence' : ''}`}
               key={index}
               onClick={() => handleClick(index, value)}
             >
@@ -115,35 +135,51 @@ function App() {
             RESET
           </button>
 
-          <button className="reset" style={{fontSize:'2rem', padding:'10px 20px'}} onClick={() => setPlay(false)}>
+          <button
+            className="reset"
+            style={{ fontSize: "2rem", padding: "10px 20px" }}
+            onClick={() => setPlay(false)}
+          >
             <IoMdArrowBack />
           </button>
         </>
       )}
 
-      {winner && (
+      {winner != "" && showWithDelay && (
         <div className="winner">
           <p>The winner is </p>
           <p>{winner}</p>
           <p style={{ fontSize: "20px", marginTop: "10px" }}>
-            click <b onClick={resetGame} style={{ color: "rgb(90, 70, 219)", cursor:'pointer' }}>RESET</b> to play
-            again
+            click{" "}
+            <b
+              onClick={resetGame}
+              style={{ color: "rgb(90, 70, 219)", cursor: "pointer" }}
+            >
+              RESET
+            </b>{" "}
+            to play again
           </p>
         </div>
       )}
 
-      {board.every(elem => elem != 0) && (
+      {board.every((elem) => elem != 0) && (
         <div className="winner">
           <p>TIE! 🥲</p>
           <p style={{ fontSize: "20px", marginTop: "10px" }}>
-            click <b onClick={resetGame} style={{ color: "rgb(90, 70, 219)", cursor:'pointer' }}>RESET</b> to play
-            again
+            click{" "}
+            <b
+              onClick={resetGame}
+              style={{ color: "rgb(90, 70, 219)", cursor: "pointer" }}
+            >
+              RESET
+            </b>{" "}
+            to play again
           </p>
         </div>
       )}
 
       <footer>
-        <p>Gabriel Barros</p>
+        <p>uGab</p>
         <a href="https://www.linkedin.com/in/ugab/" target="_blank">
           <AiFillLinkedin />
         </a>
