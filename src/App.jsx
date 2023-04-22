7f00003d949764c0041fcc34b1ca55d93cdf0ab9
ugabb <gsb.alternativo@gmail.com>
2023-04-22 00:26:52 -0300

    first commit. Game logic working and desing completed

---
+++ src/App.jsx
@@ -0,0 +1,158 @@
+import { useState, useEffect } from "react";
+
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import { IoMdArrowBack } from "react-icons/io";
+
+function App() {
+  // 0 == empty, 1 == x, 2 == o
+  const [board, setBoard] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [turn, setTurn] = useState(1);
+  const [winner, setWinner] = useState(null);
+  const [play, setPlay] = useState(false);
+
+  const winnerCombinations = [
+    // horizontal
+    { indexes: [0, 1, 2], direction: "horizontal" },
+    { indexes: [3, 4, 5], direction: "horizontal" },
+    { indexes: [6, 7, 8], direction: "horizontal" },
+
+    // vertical
+    { indexes: [0, 3, 6], direction: "vertical" },
+    { indexes: [1, 4, 7], direction: "vertical" },
+    { indexes: [2, 5, 8], direction: "vertical" },
+
+    // diagonals
+    { indexes: [0, 4, 8], direction: "diagonal-1" },
+    { indexes: [2, 4, 6], direction: "diagonal-2" },
+  ];
+
+  function handleClick(currentIndex, value) {
+    if (winner) return;
+    const newBoard = [...board];
+
+    // x turn
+    if (turn === 1) {
+      if (value === 0) {
+        newBoard[currentIndex] = 1;
+        setBoard(newBoard);
+      } else return;
+    }
+    // o turn
+    if (turn === 2) {
+      if (value === 0) {
+        newBoard[currentIndex] = 2;
+        setBoard(newBoard);
+      } else return;
+    }
+
+    // change turn
+    turn === 1 ? setTurn(2) : setTurn(1);
+  }
+
+  function checkWinner() {
+    for (let combination of winnerCombinations) {
+      const { indexes } = combination;
+
+      // if x win
+      if (
+        board[indexes[0]] === 1 &&
+        board[indexes[1]] === 1 &&
+        board[indexes[2]] === 1
+      ) {
+        setWinner("❌");
+      }
+      // if o win
+      if (
+        board[indexes[0]] === 2 &&
+        board[indexes[1]] === 2 &&
+        board[indexes[2]] === 2
+      ) {
+        setWinner("⭕");
+      }
+      if (winner) break;
+    }
+    return winner;
+  }
+
+  function resetGame() {
+    setBoard([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    setTurn(1);
+    setWinner(null);
+  }
+
+  useEffect(() => {
+    checkWinner();
+  }, [board]);
+
+  return (
+    <div className="container">
+      <div className="play-container">
+        <h1>Tic ❌ Tac ⭕ Toe</h1>
+
+        {!play && (
+          <button className="play" onClick={() => setPlay(true)}>
+            Play!
+          </button>
+        )}
+      </div>
+
+      {play && (
+        <div className="board-container">
+          {board.map((value, index) => (
+            <span
+              className={`${value ? "alreadyPlaced" : ""}`}
+              key={index}
+              onClick={() => handleClick(index, value)}
+            >
+              {value === 0 ? "" : value === 1 ? "❌" : "⭕"}
+            </span>
+          ))}
+        </div>
+      )}
+      {play && (
+        <>
+          <button className="reset" onClick={resetGame}>
+            RESET
+          </button>
+
+          <button className="reset" style={{fontSize:'2rem', padding:'10px 20px'}} onClick={() => setPlay(false)}>
+            <IoMdArrowBack />
+          </button>
+        </>
+      )}
+
+      {winner && (
+        <div className="winner">
+          <p>The winner is </p>
+          <p>{winner}</p>
+          <p style={{ fontSize: "20px", marginTop: "10px" }}>
+            click <b onClick={resetGame} style={{ color: "rgb(90, 70, 219)", cursor:'pointer' }}>RESET</b> to play
+            again
+          </p>
+        </div>
+      )}
+
+      {board.every(elem => elem != 0) && (
+        <div className="winner">
+          <p>TIE! 🥲</p>
+          <p style={{ fontSize: "20px", marginTop: "10px" }}>
+            click <b onClick={resetGame} style={{ color: "rgb(90, 70, 219)", cursor:'pointer' }}>RESET</b> to play
+            again
+          </p>
+        </div>
+      )}
+
+      <footer>
+        <p>Gabriel Barros</p>
+        <a href="https://www.linkedin.com/in/ugab/" target="_blank">
+          <AiFillLinkedin />
+        </a>
+        <a href="https://github.com/ugabb" target="_blank">
+          <AiFillGithub />
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
