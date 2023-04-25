fc1c642eec0acf26584245859a9458d65836346a
ugabb <gsb.alternativo@gmail.com>
2023-04-24 22:53:41 -0300

    score added, computer moves added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ import { IoMdArrowBack } from "react-icons/io";
 function App() {
   // 0 == empty, 1 == x, 2 == o
   const [board, setBoard] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
-  const [turn, setTurn] = useState(1);
+  const [score, setScore] = useState({
+    player: 0,
+    computer: 0,
+  });
+  // const [playerTurn]
   const [winner, setWinner] = useState(null);
   const [sequenceWinner, setSequenceWinner] = useState([]);
   const [showWithDelay, setShowWithDelay] = useState(false);
@@ -32,23 +36,28 @@ function App() {
     if (winner) return;
     const newBoard = [...board];
 
-    // x turn
-    if (turn === 1) {
-      if (value === 0) {
-        newBoard[currentIndex] = 1;
-        setBoard(newBoard);
-      } else return;
-    }
-    // o turn
-    if (turn === 2) {
-      if (value === 0) {
-        newBoard[currentIndex] = 2;
-        setBoard(newBoard);
-      } else return;
+    if (value === 0) {
+      newBoard[currentIndex] = 1;
+      setBoard(newBoard);
     }
 
-    // change turn
-    turn === 1 ? setTurn(2) : setTurn(1);
+    // if board option is empty or not a X
+    setTimeout(computerMove(newBoard), 3000);
+  }
+
+  function computerMove(board) {
+    if (winner) return;
+    let randomNum = Math.floor(Math.random() * 9);
+
+    if (board.includes(0)) {
+      while (board[randomNum] != 0) {
+        randomNum = Math.floor(Math.random() * 9);
+      }
+      board[randomNum] = 2;
+      setBoard(board);
+    } else return;
+
+    return;
   }
 
   function checkWinner() {
@@ -56,7 +65,6 @@ function App() {
       const { indexes } = combination;
 
       // if x win
-      // console.log(indexes[0],indexes[1],indexes[2]);
 
       if (
         board[indexes[0]] === 1 &&
@@ -64,7 +72,13 @@ function App() {
         board[indexes[2]] === 1
       ) {
         setWinner("❌");
-        setSequenceWinner((prev) => [...prev, indexes[0],indexes[1],indexes[2]]);
+        setScore((prev) => ({ ...prev, player: prev.player + 1 }));
+        setSequenceWinner((prev) => [
+          ...prev,
+          indexes[0],
+          indexes[1],
+          indexes[2],
+        ]);
         // winner alert will show with 2 seconds of delay
         setTimeout(() => {
           setShowWithDelay(true);
@@ -77,7 +91,13 @@ function App() {
         board[indexes[2]] === 2
       ) {
         setWinner("⭕");
-        setSequenceWinner((prev) => [...prev, indexes[0],indexes[1],indexes[2]]);
+        setScore((prev) => ({ ...prev, computer: prev.computer + 1 }));
+        setSequenceWinner((prev) => [
+          ...prev,
+          indexes[0],
+          indexes[1],
+          indexes[2],
+        ]);
         // winner alert will show with 2 seconds of delay
         setTimeout(() => {
           setShowWithDelay(true);
@@ -88,16 +108,11 @@ function App() {
     return winner;
   }
 
-  useEffect(() => {
-    console.log("Sequence winner:", sequenceWinner);
-  }, [sequenceWinner]);
-
   function resetGame() {
     setBoard([0, 0, 0, 0, 0, 0, 0, 0, 0]);
-    setTurn(1);
     setWinner(null);
     setShowWithDelay(false);
-    setSequenceWinner([])
+    setSequenceWinner([]);
   }
 
   useEffect(() => {
@@ -108,6 +123,11 @@ function App() {
     <div className="container">
       <div className="play-container">
         <h1>Tic ❌ Tac ⭕ Toe</h1>
+
+        <div className="score-container">
+          <p>Player: <b>{score.player}</b></p>
+          <p>Computer: <b>{score.computer}</b></p>
+        </div>
 
         {!play && (
           <button className="play" onClick={() => setPlay(true)}>
@@ -120,7 +140,9 @@ function App() {
         <div className="board-container">
           {board.map((value, index) => (
             <span
-              className={`${value ? "alreadyPlaced" : ""} ${sequenceWinner.includes(index) ? 'winnerSequence' : ''}`}
+              className={`${value ? "alreadyPlaced" : ""} ${
+                sequenceWinner.includes(index) ? "winnerSequence" : ""
+              }`}
               key={index}
               onClick={() => handleClick(index, value)}
             >
@@ -138,7 +160,10 @@ function App() {
           <button
             className="reset"
             style={{ fontSize: "2rem", padding: "10px 20px" }}
-            onClick={() => setPlay(false)}
+            onClick={() => {
+              setPlay(false);
+              setScore((prev) => ({ player: 0, computer: 0 }));
+            }}
           >
             <IoMdArrowBack />
           </button>
@@ -162,7 +187,8 @@ function App() {
         </div>
       )}
 
-      {board.every((elem) => elem != 0) && (
+      {/* IF IS TIE */}
+      {board.every((elem) => elem != 0 && !winner) && (
         <div className="winner">
           <p>TIE! 🥲</p>
           <p style={{ fontSize: "20px", marginTop: "10px" }}>
